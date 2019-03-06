e69991c9e4cc3744914b068f60d33a39fd9731d3
botnyx <chlsbkrs@gmail.com>
2019-03-06 21:40:50 +0100

    Update dependencies.php

--- src/includes/dependencies.php
+++ src/includes/dependencies.php
@@ -1,18 +1,20 @@
 <?php
 
 
-_SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/dependencies.php"
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-shared-core/src/includes/dependencies.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeBackend',_SETTINGS) &&  file_exists($_include)){
+	//error_log('/src/backend_dependencies.php');
+	require_once($_include);
+}
+
 
 /* Load dependency related stuff */
 #if(array_key_exists('sfeAuth',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/oauth_dependencies.php')){
 #	error_log('/src/oauth_dependencies.php');
 #	require_once(_SETTINGS['paths']['root'] .'/src/oauth_dependencies.php');
 #}
-/* Load backend related stuff */
-if(array_key_exists('sfeBackend',_SETTINGS) &&  file_exists(_SETTINGS['paths']['root'] .'/src/backend_dependencies.php')){
-	error_log('/src/backend_dependencies.php');
-	require_once(_SETTINGS['paths']['root'] .'/src/backend_dependencies.php');
-}
+
 /* Load backend related stuff */
 #if(array_key_exists('sfeCdn',_SETTINGS) &&  file_exists(_SETTINGS['paths']['root'] .'/src/cdn_dependencies.php')){
 #	error_log('/src/cdn_dependencies.php');
