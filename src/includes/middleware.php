9fa3bf311c429c71fb1aad08f8aebcce1f52d6e8
botnyx <chlsbkrs@gmail.com>
2019-03-06 22:16:56 +0100

    ..

--- src/includes/middleware.php
+++ src/includes/middleware.php
@@ -2,17 +2,29 @@
 
 /* Load middleware related stuff */
 
-#if(array_key_exists('sfeAuth',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/oauth_middleware.php')){
-#	error_log('/src/oauth_middleware.php');
-#	require_once(_SETTINGS['paths']['root'] .'/src/oauth_middleware.php');
-#}
-/* Load backend related stuff */
-if(array_key_exists('sfeBackend',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/backend_middleware.php')){
-	error_log('/src/backend_middleware.php');
-	require_once(_SETTINGS['paths']['root'] .'/src/backend_middleware.php');
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-auth-core/src/includes/middleware.php";
+if(array_key_exists('sfeAuth',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
 }
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-frontend-core/src/includes/middleware.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeFrontend',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
+
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-backend-core/src/includes/middleware.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeBackend',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-cdn-core/src/includes/middleware.php";
 /* Load cdn related stuff */
-#if(array_key_exists('sfeCdn',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/cdn_middleware.php')){
-#	error_log('/src/cdn_middleware.php');
-#	require_once(_SETTINGS['paths']['root'] .'/src/cdn_middleware.php');
-#}
+if(array_key_exists('sfeCdn',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
--- src/includes/routes.php
+++ src/includes/routes.php
@@ -5,22 +5,30 @@
 #	error_log('/src/oauth_routes.php');
 #	require_once(_SETTINGS['paths']['root'] .'/src/oauth_routes.php');
 #}
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-backend-core/src/includes/routes.php";
 /* Load backend related stuff */
-if(array_key_exists('sfeBackend',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/backend_routes.php')){
-	error_log('/src/backend_routes.php');
-	require_once(_SETTINGS['paths']['root'] .'/src/backend_routes.php');
+if(array_key_exists('sfeBackend',_SETTINGS) && file_exists( $_include )){
+	require_once($_include);
 }
 
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-cdn-core/src/includes/routes.php";
 /* Load Cdn related stuff */
-#if(array_key_exists('sfeCdn',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/cdn_routes.php')){
-#	error_log('/src/cdn_routes.php');
-#	require_once(_SETTINGS['paths']['root'] .'/src/cdn_routes.php');
-#}
+if(array_key_exists('sfeCdn',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
 
-
-
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-auth-core/src/includes/routes.php";
 /* Load Cdn related stuff */
-#if(array_key_exists('sfeFrontend',_SETTINGS) && file_exists(_SETTINGS['paths']['root'] .'/src/frontend_routes.php')){
-#	error_log('/src/frontend_routes.php');
-#	require_once(_SETTINGS['paths']['root'] .'/src/frontend_routes.php');
-#}
+if(array_key_exists('sfeAuth',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
+
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-frontend-core/src/includes/routes.php";
+/* Load Cdn related stuff */
+if(array_key_exists('sfeFrontend',_SETTINGS) && file_exists($_include)){
+	require_once($_include);
+}
