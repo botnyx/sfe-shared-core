8ebaa14f20620532755986204ae28d25adcf9401
botnyx <chlsbkrs@gmail.com>
2019-03-07 00:53:40 +0100

    Create container.php

---
+++ src/includes/container.php
@@ -0,0 +1,32 @@
+<?php
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-backend-core/src/includes/container.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeBackend',_SETTINGS) &&  file_exists($_include)){
+	//error_log('/src/backend_dependencies.php');
+	require_once($_include);
+}
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-frontend-core/src/includes/container.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeFrontend',_SETTINGS) &&  file_exists($_include)){
+#	error_log('/src/cdn_dependencies.php');
+	require_once($_include);
+}
+
+
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-auth-core/src/includes/container.php";
+/* Load dependency related stuff */
+if(array_key_exists('sfeAuth',_SETTINGS) && file_exists($_include)){
+#	error_log('/src/oauth_dependencies.php');
+	require_once($_include);
+}
+
+$_include = _SETTINGS['paths']['root']."/vendor/botnyx/sfe-cdn-core/src/includes/container.php";
+/* Load backend related stuff */
+if(array_key_exists('sfeCdn',_SETTINGS) &&  file_exists($_include)){
+#	error_log('/src/cdn_dependencies.php');
+	require_once($_include);
+}
