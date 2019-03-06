10debab7fee058a93e432801eb6fbf572fb2be02
botnyx <chlsbkrs@gmail.com>
2019-03-07 00:35:37 +0100

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -4,4 +4,48 @@ namespace Botnyx\Sfe\Shared;
 
 class Application {
 	
+	
+	function __construct($_settings){
+		$this->settings = $_settings;
+	}
+	
+	/* Create the Slim application */
+	public function start(){
+		
+		$this->app = new \Slim\App([
+			'debug' => $this->settings['slim']['debug'],
+			'settings' => [
+
+				'displayErrorDetails' => $this->settings['slim']['debug'], // set to false in production
+				// Monolog settings
+				'logger' => [
+					'name' => 'slim-app',
+					'path' => isset($_ENV['docker']) ? 'php://stdout' : $this->settings['paths']['logs'].'/app.log',
+					'level' => \Monolog\Logger::DEBUG,
+				],
+				'addContentLengthHeader'=>false,  
+		/*		'addContentLengthHeader'=>false 
+					ALWAYS disable this, else  the error 
+					PHP Fatal error:  Uncaught TypeError: fread() expects parameter 2 to be integer, string given 
+					Zend\\Diactoros\\Stream->read('173') 
+				*/
+			],
+		]);
+		return $this->app;		
+	}
+	
+	/* Cookie helper func. */
+	public function getCookieValue(\Slim\Http\Request $request, $cookieName)
+	{
+		$cookies = $request->getCookieParams();
+		return isset($cookies[$cookieName]) ? $cookies[$cookieName] : null;
+	}
+
+	/* dev helper to show errors. */
+	public function show_errors(){
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL ^ E_NOTICE);
+	}
+
 }
