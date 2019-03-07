7b62b78bd3945552822d43a1a9fab79696e47615
botnyx <chlsbkrs@gmail.com>
2019-03-07 14:24:20 +0100

    Update Util.php

--- src/Util.php
+++ src/Util.php
@@ -3,14 +3,12 @@
 namespace Botnyx\Sfe\Shared;
 
 class Util {
-	
+
 	/* Cookie helper func. */
-	public function getCookieValue(\Slim\Http\Request $request, $cookieName)
+	static function getCookieValue(\Slim\Http\Request $request, $cookieName)
 	{
 		$cookies = $request->getCookieParams();
 		return isset($cookies[$cookieName]) ? $cookies[$cookieName] : null;
 	}
-	
+
 }
-	
-	
