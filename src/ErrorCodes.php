593afdb1939988d745b9368be54ccd1497e2fb2b
botnyx <chlsbkrs@gmail.com>
2019-03-09 16:59:27 +0100

    Update ErrorCodes.php

--- src/ErrorCodes.php
+++ src/ErrorCodes.php
@@ -3,40 +3,74 @@
 namespace Botnyx\Sfe\Shared;
 
 class ErrorCodes{
+	/*
+		Sfe error codes.
+		
+		
+		Slim reponse usage:
+		return \Botnyx\Sfe\Shared\ExceptionResponse::get($response,2201);
+		
+		raise Exception (only the code):
+		new Botnyx\Sfe\Shared\Exception($msg,$code)
 	
+	
+	*/
 	function __construct(){
 		
 	}
 	
 	function get($code){
+		
 		$array = array();
-		
-		
-		
+		/* Merge the arrays into 1 big one. */
 		$array = array_merge($array,$this->configErrors());
 		
-		//print_r($array);
-		
+		/* return the error */
 		return $array["E".$code];
 	}
 	/*
-		
 		Configuration Errors
-			
-			
 		Connection Errors
-		
 		Runtime Errors
-	
-	
-	
-		Frontend
-		Backend 
-		Authsrv
 	
 	*/
 	
+	/*
+		Application 1-99
+		Frontend  100-199
+		Backend   200-299
+		Authsrv   300-399
+	
+	*/
+	function filesystemErrors(){
+		/*
+		Application 1-99
+		Frontend  100-199
+		Backend   200-299
+		Authsrv   300-399
+		
+			filesystem errors between 2000-2999
+		*/
+		return array(
+			"E2200"=>"no templatepath found for client",
+			"E2201"=>"Origin template not found for client",
+			"E2202"=>"FATAL vendor/botnyx/backend-core/templates not found!"
+			
+		
+		);
+	}
+	
+	
 	function configErrors(){
+		/*
+		Application 1-99
+		Frontend  100-199
+		Backend   200-299
+		Authsrv   300-399
+		
+			config errors between 1000-1999
+		*/
+		
 		return array(
 			"E1001"=>"missing configuration.ini",
 			"E1002"=>"missing `root` path.",
@@ -75,6 +109,8 @@ class ErrorCodes{
 			
 		);
 	}
+	
+	
 	
 	
 	function generic (){
